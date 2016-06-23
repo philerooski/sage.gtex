fa80a1fa24ebec2ac6b49e480b937cf5dcef9eb8
Phil Snyder <subfivemile@gmail.com>
2016-06-23 14:10:55 -0700

    conformance to gct format

--- geneCountMunge.R
+++ geneCountMunge.R
@@ -3,8 +3,8 @@
 
 library(limma)
 
-filename <- "~/gtex/short_gtex.gct" #commandArgs(trailingOnly = T)[2]
-destination <- "output.gct" #commandArgs(trailingOnly = T)[3]
+filename <- commandArgs(trailingOnly = T)[2]
+destination <- commandArgs(trailingOnly = T)[3]
 gene_data <- read.table(filename, header=T)
 gene_colnames <- colnames(gene_data)
 
@@ -22,6 +22,6 @@ keep_row <- by(gene_data, 1:nrow(gene_data), sparse_gene)
 gene_data <- gene_data[keep_row,] # remove sparse genes
 voom_normalized_counts <- voom(gene_data[,3:ncol(gene_data)]) # voom normalization
 gene_data <- cbind(gene_data[,c(1,2)], voom_normalized_counts$weights)
-write(gene_colnames, destination, ncolumns = ncol(gene_data))
-write.table(gene_data, destination, 
+write(gene_colnames, destination, ncolumns = ncol(gene_data), sep="\t")
+write.table(gene_data, destination, sep="\t",
 	append=T, col.names=F, row.names=F, quote=F)
