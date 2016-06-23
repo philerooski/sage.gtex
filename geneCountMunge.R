79da56adba447dd114dadf457406a0c39dd762e2
Phil Snyder <subfivemile@gmail.com>
2016-06-23 13:35:35 -0700

    data frame iteration optimizations

--- geneCountMunge.R
+++ geneCountMunge.R
@@ -3,20 +3,22 @@
 
 library(limma)
 
-filename <- commandArgs(trailingOnly = T)[2]
-destination <- commandArgs(trailingOnly = T)[3]
+filename <- "~/gtex/short_gtex.gct" #commandArgs(trailingOnly = T)[2]
+destination <- "output.gct" #commandArgs(trailingOnly = T)[3]
 gene_data <- read.table(filename, header=T)
 gene_colnames <- colnames(gene_data)
-keep_row <- c()
-for (i in 1:length(gene_data$Name)) {
-	counts <- as.numeric(gene_data[i,3:ncol(gene_data)])
+
+sparse_gene <- function(gene_row) {
+	counts <- as.numeric(gene_row[3:length(gene_row)])
 	num_zero <- length(which(counts < 1))
 	if (num_zero > ncol(gene_data) / 2) {
-		keep_row <- c(keep_row, F)	
+		return(F)
 	} else {
-		keep_row <- c(keep_row, T)	
+		return(T)
 	}
 }
+
+keep_row <- by(gene_data, 1:nrow(gene_data), sparse_gene)
 gene_data <- gene_data[keep_row,] # remove sparse genes
 voom_normalized_counts <- voom(gene_data[,3:ncol(gene_data)]) # voom normalization
 gene_data <- cbind(gene_data[,c(1,2)], voom_normalized_counts$weights)
